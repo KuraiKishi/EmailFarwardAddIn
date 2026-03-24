cf3b1699df615c3628cb9c3c76bbdc79a2380b3d
KuraiKishi <61376348+KuraiKishi@users.noreply.github.com>
2026-03-25 02:51:29 +0300

    Add additional recipient to phishing report email

--- taskpane.js
+++ taskpane.js
@@ -18,7 +18,7 @@ var xHeaderRecipientUUID = "X-FORTIPHISH-RECIPIENT-UUID";
 var itemId = "";
 var mimeContent = "";
 var subject = "Reported Phishing Email";
-var recipients = ["a.alsaleh@ahda.gov.sa"];
+var recipients = ["a.alsaleh@ahda.gov.sa, rp@ahda.gov.sa"];
 var subjectPrefix = "[Phish Alert]";
 var emailBody =
   "I would like to report the email enclosed as a phishing email.";
